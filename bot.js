a7ab45f8f6e84b7f045f2fb031beb10ade3a71bc
Raaxe <mrxhm1555@gmail.com>
2018-11-14 21:33:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("انا بوت ابن كلب",{type: 'WATCHING'})
+   client.user.setActivity("",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
